a42a6fcb968a66e36762b2f63927046078015ade
Rosember10 <rosember.01@outlook.com>
2023-09-28 17:44:50 -0400

    corrector projet

---
+++ corrector-back-end/app/Services/GTPService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+/**
+ * Class GTPService.
+ */
+class GTPService
+{
+
+    protected $apiEndpoint = 'https://api.openai.com/v1/chat/completions';
+    
+}
