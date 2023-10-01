34d6973b21738300e754491ab20234672ea8182d
Rosember10 <rosember.01@outlook.com>
2023-10-01 00:07:42 -0400

    creation of endpoints for correction

--- corrector-back-end/app/Http/Controllers/GPTController.php
+++ corrector-back-end/app/Http/Controllers/GPTController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+use function Laravel\Prompts\text;
 
 class GPTController extends Controller
 {
-    //
+    protected $URL_API;
+    protected $TOKEN_API;
+
+    public function __construct()
+    {
+        $this->URL_API = 'https://api.openai.com/v1/chat/completions';
+        $this->TOKEN_API = env('OPENAI_API_KEY');
+    }
+
+
+    public function correctFrench(Request $request)
+    {
+        $text = $request->text;
+        $language = $request->language;
+
+        $response = Http::withoutVerifying() // para evitar la verificacion de certificado ssl 
+            ->withHeaders([
+                'Authorization' => 'Bearer ' . env('OPENAI_API_KEY'),
+                'Content-Type' => 'application/json',
+            ])->post($this->URL_API, [
+                "model" => "gpt-3.5-turbo",
+                "messages" => [
+                    [
+                        "role" => "user",
+                        "content" => "Traduce al".$language. " y corrige esta frase:" . $text
+                    ]
+                ],
+                "temperature" => 0.5,
+                'max_tokens' => 1000,
+            ]);
+        $content = $response['choices'][0]['message']['content'];
+        return response()->json([
+            'content' => $content
+        ]);
+    }
 }
--- corrector-back-end/routes/api.php
+++ corrector-back-end/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GPTController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('correctfrench',[GPTController::class,'correctFrench']);
+Route::get('test',[GPTController::class,'test']);
