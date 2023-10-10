f79269acff355419d5e171e105aea936b77b8678
Rosember10 <rosember.01@outlook.com>
2023-10-10 14:03:18 -0400

    start of css with the header and font

--- corrector-front-end/src/components/Header/Header.tsx
+++ corrector-front-end/src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
-
+import './Header.css'
 
 function Header() {
     return (
         <header>
             <h1>CORRECTEUR DE GRAMMAIRE</h1>
-            <p>"Correcteur de Français: Votre assitant pour une orthographe et una grammaire irréprochables. Apprenez en toute simplicité!"</p>
+            <p>"Correcteur de Français: Votre assitant pour une orthographe et une grammaire irréprochables. Apprenez en toute simplicité!"</p>
         </header>
     )
 }
