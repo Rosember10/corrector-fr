c879091405d90a0fa9a08b89f664ceadc46d8dab
Rosember10 <rosember.01@outlook.com>
2023-10-14 11:25:17 -0400

    fix section about color and img

--- corrector-front-end/src/components/About/About.tsx
+++ corrector-front-end/src/components/About/About.tsx
@@ -1,21 +1,20 @@
 import './About.css'
 import Typed from '../Typed/Typed'
 import gramm from '../../assets/gramm.png';
-import gramm2 from '../../assets/gramm2.png';
-import gramm3 from '../../assets/gramm3.png';
+
 
 export default function About() {
 
     return (
-        <article className='about'>
+        <section className='about'>
+            <h2>Laissez-moi être la touche finale à chaque phrase que vous écrivez.</h2>
             <div className="about_text">
-                <p>Laissez-moi être la touche finale à chaque phrase que vous écrivez.</p>
-                <p> - <Typed text=" Je suis ici pour t'aider " delay={100} infinite /></p>
-                <p className='about_name'>Mon nom est gramm </p>
+                <p>     
+                    <Typed text="Salut, Je suis ici pour t'aider " delay={150} infinite />  
+                    <span id='pipe' > | </span> 
+                </p> 
+                <img src={gramm} alt="" />
             </div>
-            <img src={gramm} alt="" />
-        </article>
-        
-
+        </section>
     )
 }
